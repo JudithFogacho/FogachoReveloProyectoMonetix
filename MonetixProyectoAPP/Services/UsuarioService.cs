d5872c0f59082649a792738a6a692e6b3c5942ee
Judith Alejandra Fogacho Suquilanda <judith.fogacho@udla.edu.ec>
2025-01-13 18:04:58 -0500

    Creación del UsuarioService, LoginViewModel y sus metodos

--- MonetixProyectoAPP/Services/UsuarioService.cs
+++ MonetixProyectoAPP/Services/UsuarioService.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.Maui.Storage;
 
 namespace MonetixProyectoAPP.Services
 {
-    internal class UsuarioService
+    public class UsuarioService
     {
+        private readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:7156/api/")
+        };
+
+        public async Task<string> LoginAsync(string email, string password) {
+            var credenciales = new { Email = email, Password = password };
+
+            var respuesta = await _httpClient.PostAsJsonAsync("Usuario/login", credenciales);
+
+            if (respuesta.IsSuccessStatusCode) {
+                var tokenResponse = await respuesta.Content.ReadFromJsonAsync<TokenResponse>();
+                return tokenResponse?.Token;
+            }
+            return null;
+        }
+
+        public HttpClient GetAutheticatedHttpClient() {
+            var token = Preferences.Get("auth_token", string.Empty);
+            if (token == null) {
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+            return _httpClient;
+        }
+
+        public class TokenResponse { 
+            public string Token { get; set; }
+        }
     }
 }
--- MonetixProyectoAPP/ViewModels/LoginViewModel.cs
+++ MonetixProyectoAPP/ViewModels/LoginViewModel.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using MonetixProyectoAPP.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MonetixProyectoAPP.Models;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+using MonetixProyectoAPP.Views;
 
 namespace MonetixProyectoAPP.ViewModels
 {
-    internal class LoginViewModel
+    public class LoginViewModel: INotifyPropertyChanged
     {
+        private readonly UsuarioService _service = new UsuarioService();
+
+        private string _email;
+        private string _password;
+
+        public string Email {
+            get => _email; 
+            set { 
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //Creación de metodos para que nos permita enlazar acciones en la interfaz de usuario
+        public ICommand loginCommand { get;}
+
+        public LoginViewModel() {
+            loginCommand = new Command(async () => await LoginAsync());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private async Task LoginAsync() {
+
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password) ) {
+                await Application.Current.MainPage.DisplayAlert("ERROR", "Por favor ingrese email y contraseña", "OK");
+                return;
+            }
+
+            try {
+                var token = await _service.LoginAsync(Email, Password);
+                if (!string.IsNullOrEmpty(token))
+                {
+                    Preferences.Set("auth_token", token);
+                    await Application.Current.MainPage.Navigation.PushAsync(new PaginaInicial());
+                }
+                else { 
+                    await Application.Current.MainPage.DisplayAlert("ERROR", "Credenciales Incorrectas", "OK");
+                }
+            } catch (Exception ex) {
+
+                await Application.Current.MainPage.DisplayAlert("ERROR", $"Ocurrio un error: {ex.Message}", "OK");
+            }
+        }
+
     }
 }
