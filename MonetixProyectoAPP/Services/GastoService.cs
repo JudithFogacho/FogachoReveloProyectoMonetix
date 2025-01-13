582d22afc3e8c1ffa607d5bac73f03a039e13238
Judith Alejandra Fogacho Suquilanda <judith.fogacho@udla.edu.ec>
2025-01-13 17:19:20 -0500

    Creacion De GastoViewModel, Gastos Services y sus metodos

--- MonetixProyectoAPP/Services/GastoService.cs
+++ MonetixProyectoAPP/Services/GastoService.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using MonetixProyectoAPP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Net.Http;
 
 namespace MonetixProyectoAPP.Services
 {
-    internal class GastoService
+    public class GastoService
     {
+        private readonly HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:7156/api/")
+        };
+
+        public async Task <List<Gasto>> GetGastosAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Gasto>>("Gasto")?? new List<Gasto> ();
+
+        }
+
+        public async Task CreateGastoAsync (Gasto nuevoGasto)
+        {
+            await _httpClient.PostAsJsonAsync("Gasto", nuevoGasto);
+        }
+
+        public async Task DeleteGastoAsync(int idGasto)
+        {
+            await _httpClient.DeleteAsync($"Gasto/{idGasto}");
+        }
+
     }
 }
--- MonetixProyectoAPP/ViewModels/GastoViewModel.cs
+++ MonetixProyectoAPP/ViewModels/GastoViewModel.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using MonetixProyectoAPP.Models;
+using MonetixProyectoAPP.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonetixProyectoAPP.ViewModels
 {
-    internal class GastoViewModel
+    public class GastoViewModel: INotifyPropertyChanged
     {
+        private readonly GastoService _service = new GastoService();
+
+        private List<Gasto> _gasto = new List<Gasto>();
+
+        public List<Gasto> Gastos { get => _gasto; set { _gasto = value; OnPropertyChanged(); } }
+
+        public GastoViewModel() { 
+            LoadGastos ();
+        
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null) {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private async Task LoadGastos()
+        {
+            var gastos = await _service.GetGastosAsync();
+            Gastos.Clear ();
+            foreach (var gasto in gastos)
+            {
+                Gastos.Add (gasto);
+            }
+            
+        }
+
+        public async Task IngresarGasto (Gasto nuevoGasto)
+        {
+            await _service.CreateGastoAsync(nuevoGasto);
+            await LoadGastos();
+        }
+
+        public async Task EliminarGasto(int idGasto)
+        {
+            await _service.DeleteGastoAsync(idGasto);
+            await LoadGastos();
+        }
+
+
     }
 }
